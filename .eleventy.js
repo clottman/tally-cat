f66717eb495729d3b06d48e7ae82cdfaef7b28ee
Cassey Lottman <clottman@users.noreply.github.com>
2021-07-30 17:57:13 -0500

    set up eleventy image plugin

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,25 @@
+const Image = require("@11ty/eleventy-img");
+
+async function imageShortcode(src, alt, sizes) {
+  let metadata = await Image(src, {
+    widths: [150, 300],
+    formats: ["webp", "jpeg"]
+  });
+
+  let imageAttributes = {
+    alt,
+    sizes,
+    loading: "lazy",
+    decoding: "async",
+  };
+
+  // You bet we throw an error on missing alt in `imageAttributes` (alt="" works okay)
+  return Image.generateHTML(metadata, imageAttributes);
+}
+
 module.exports = function(eleventyConfig) {
-  
+    eleventyConfig.addNunjucksAsyncShortcode("image", imageShortcode);
+    eleventyConfig.addPassthroughCopy('img/');
     // eleventyConfig.addFilter( "myFilter", function() {});
    eleventyConfig.setTemplateFormats(["njk", "ejs", "js", "css"]);
     return {
